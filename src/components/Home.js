184a9295a06c143ac51538d265118f590dca57cc
Rakesh Kadam <rakesh22kadam@outlook.com>
2020-12-16 09:46:14 +0530

    Done With HomePage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,13 @@
 import react from 'react';
+import image from "../bk2.jpg";
 
 export default function Home() {
-    return <h1>Home Page!</h1>
+    return (
+        <main>
+            <img src={image} alt="Bk Image" className="absolute object-cover w-full h-full"/>
+            <section className="relative flex min-h-screen pt-8 lg:pt-10 px-8">
+                <h1 className="text-6xl text-green-500 font-bold cursive leading-none lg:leading-snug  home-name">Hii. I'm Rakesh</h1>
+            </section>
+        </main>
+    )
 }
