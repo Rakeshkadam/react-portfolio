04af5da7e64c1c5d409d5305c8c44b5ca0a550af
Rakesh Kadam <rakesh22kadam@outlook.com>
2020-12-16 15:39:53 +0530

    done with project page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,8 @@ export default function Home() {
         <main>
             <img src={image} alt="Bk Image" className="absolute object-cover w-full h-full"/>
             <section className="relative flex min-h-screen pt-8 lg:pt-10 px-8">
-                <h1 className="text-6xl text-green-500 font-bold cursive leading-none lg:leading-snug  home-name">Hii. I'm Rakesh</h1>
+                <h1 className="text-6xl text-green-500 font-bold cursive leading-none lg:leading-snug  home-name">Hii. I'm Rakesh <br/> <span className=" text-6xl text-green-500 font-bold cursive lg:leading-snug  home-name1">I am enthusiastic developer, learner, teamplayer</span></h1>
+                {/* <h3 className="text-green-500 font-bold cursive lg:leading-snug  home-name1">I am enthusiastic developer, learner, teamplayer</h3> */}
             </section>
         </main>
     )
