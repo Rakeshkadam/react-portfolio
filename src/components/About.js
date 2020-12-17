b54e3a8517c621d12696168fbaa27c69a130fa6d
Rakesh Kadam <rakesh22kadam@outlook.com>
2020-12-17 15:56:07 +0530

    done some changes

--- src/components/About.js
+++ src/components/About.js
@@ -31,10 +31,10 @@ export default function About() {
 
     return (
         <main className="relative">
-            <img src={image} className="absolute w-full"/>
+            <img src={image} className="absolute w-full" alt="background image"/>
             <div className="p-40 pt-20 container mx-auto relative"> 
                 <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
-                    <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"/>
+                    <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name}/>
                     <div className="text-lg flex flex-col justify-center">
                         <h1 className="cursive text-6xl text-green-300 mb-4">
                             Hey there, I'm{" "}
